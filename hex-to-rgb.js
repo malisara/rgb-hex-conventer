8d2cf4b1406959efdad402d65b92fef3d93c2bc8
Sara Mali <mali.sara7@gmail.com>
2023-02-03 20:15:06 +0100

    addressing the comment

--- hex-to-rgb.js
+++ hex-to-rgb.js
@@ -1,19 +1,21 @@
-const hexArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+"use strict";
 
+const hexArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const random_color_btn = document.getElementById('random-color-btn');
 random_color_btn.addEventListener('click', function () {
     let randomHexColor = '#';
 
     for (let i = 0; i < 6; i++) {
-        randomHexColor += hexArray[getRandomInteger()];
+        randomHexColor += hexArray[getRandomHexArrayIndex()];
     }
 
-    document.getElementById('circle').style.backgroundColor = randomHexColor
-    document.getElementById('hexInput').value = randomHexColor
+    document.getElementById('circle').style.backgroundColor = randomHexColor;
+    document.getElementById('hexInput').value = randomHexColor;
     // TODO: create rgb code from hex
 }
 
-)
+);
 
-function getRandomInteger() {
+function getRandomHexArrayIndex() {
     return Math.floor(Math.random() * hexArray.length);
 }
