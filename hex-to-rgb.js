603c479cdd5b8c92b0b1473435a1b27a83e7ed4e
Sara Mali <mali.sara7@gmail.com>
2023-02-09 11:02:51 +0100

    hex to rgb calculation

--- hex-to-rgb.js
+++ hex-to-rgb.js
@@ -11,11 +11,55 @@ random_color_btn.addEventListener('click', function () {
 
     document.getElementById('circle').style.backgroundColor = randomHexColor;
     document.getElementById('hexInput').value = randomHexColor;
-    // TODO: create rgb code from hex
+    document.getElementById('rgbInput').value = HexToRgb(randomHexColor);
 }
 
 );
 
 function getRandomHexArrayIndex() {
     return Math.floor(Math.random() * hexArray.length);
+}
+
+
+function HexToRgb(HexCode) {
+    let rgbArray = HexToRgbArray(HexCode);
+    let rgbValue = 'rgb(';
+    for (let i = 0; i < rgbArray.length - 1; i++) {
+        rgbValue += rgbArray[i] + ', ';
+    }
+    rgbValue += rgbArray[rgbArray.length - 1] + ')';
+    return rgbValue;
+}
+
+
+function HexToRgbArray(HexCode) {
+    const hexPartial = ["A", "B", "C", "D", "E", "F"];
+    const rgbPartial = [10, 11, 12, 13, 14, 15];
+    let rgbArray = [];
+
+    for (let i = 1; i < HexCode.length - 1; i += 2) {
+
+        let firstHexElement = HexCode[i];
+        let secondHexElement = HexCode[i + 1];
+        let firstElementIntoRgb = '';
+        let secondElementIntoRgb = '';
+
+        if (isNaN(firstHexElement)) {
+            firstElementIntoRgb += rgbPartial[hexPartial.indexOf(HexCode[i].toUpperCase())];
+        }
+        else {
+            firstElementIntoRgb += HexCode[i];
+        }
+
+        if (isNaN(secondHexElement)) {
+            secondElementIntoRgb += rgbPartial[hexPartial.indexOf(HexCode[i + 1].toUpperCase())];
+        }
+
+        else {
+            secondElementIntoRgb += HexCode[i + 1];
+        }
+
+        rgbArray.push(Number(firstElementIntoRgb) * 16 + Number(secondElementIntoRgb));
+    }
+    return rgbArray;
 }
